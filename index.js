dad3b992243092246b902e80865fa1ed67598308
Gage Johnson <gagejohnson117@gmail.com>
2026-02-01 18:13:26 -0600

    Git failing to push network error
    Git failing to push network error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length;
 }
 
 /**
@@ -11,7 +11,11 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // TODO
+  let sum = 0;
+  for (const numb of numbers) {
+    sum += numb;
+  }
+  return sum;
 }
 
 /**
@@ -19,7 +23,7 @@ function getSum(numbers) {
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
-  // TODO
+  return getSum(numbers) / getLength(numbers);
 }
 
 /**
@@ -27,7 +31,13 @@ function getMean(numbers) {
  * @returns {number} the smallest of the numbers
  */
 function getMin(numbers) {
-  // TODO
+  let min = numbers[0];
+  for (const numb of numbers) {
+    if (numb < min) {
+      min = numb;
+    }
+  }
+  return min;
 }
 
 /**
@@ -35,7 +45,13 @@ function getMin(numbers) {
  * @returns {number} the largest of the numbers
  */
 function getMax(numbers) {
-  // TODO
+  let max = numbers[0];
+  for (const numb of numbers) {
+    if (numb > max) {
+      max = numb;
+    }
+  }
+  return max;
 }
 
 /**
@@ -43,7 +59,7 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
-  // TODO
+  return getMax(numbers) - getMin(numbers);
 }
 
 /**
@@ -51,7 +67,13 @@ function getRange(numbers) {
  * @returns {number[]} the even numbers in the array
  */
 function getEvens(numbers) {
-  // TODO
+  let evens = [];
+  for (const numb of numbers) {
+    if (numb % 2 === 0) {
+      evens.push(numb);
+    }
+  }
+  return evens;
 }
 
 /**
@@ -59,7 +81,13 @@ function getEvens(numbers) {
  * @returns {number[]} the odd numbers in the array
  */
 function getOdds(numbers) {
-  // TODO
+  let odds = [];
+  for (const numb of numbers) {
+    if (numb % 2 !== 0) {
+      odds.push(numb);
+    }
+  }
+  return odds;
 }
 
 // === READ BUT DO NOT EDIT THE CODE BELOW ===
@@ -99,7 +127,7 @@ function describeNumbers(numbers) {
 
 const userInputString = prompt(
   "Please enter some integers separated by commas.",
-  "28,-15,30,975,400"
+  "28,-15,30,975,400",
 );
 const numbers = convertStringToNumbers(userInputString);
 describeNumbers(numbers);
